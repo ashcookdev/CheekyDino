c36f82efb6b63d647e8384e67559a55eff9f9c23
ashley cook <ashcookdev>
2023-08-19 00:56:28 +0100

    error fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "cheekydino": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
   "api": {
     "CheekyDino": {
       "GraphQLAPIEndpointOutput": "string",
