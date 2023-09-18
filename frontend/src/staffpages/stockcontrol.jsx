9976939b7aa376ab64547c47868afdc3a8fc13d3
ashley cook <ashcookdev>
2023-09-18 13:49:36 +0100

    new commit

--- frontend/src/staffpages/stockcontrol.jsx
+++ frontend/src/staffpages/stockcontrol.jsx
@@ -261,14 +261,16 @@ function handleSelectExtras(event) {
               <label htmlFor="time" className="block text-sm font-medium leading-6 text-center text-gray-900">
 Estimated Prep Time              </label>
               <div className="mt-2">
-                <input onChange={handleTimeChange}
-                  type="time"
-                  name="time"
-                  id="time"
-                  step="60" // Set to 1 minute
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="https://www.google.com/"
-                />
+              <input
+  onChange={handleTimeChange}
+  type="text"
+  name="time"
+  id="time"
+  pattern="([01]?[0-9]|2[0-3]):[0-5][0-9]" // Specify a pattern for HH:MM format
+  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+  placeholder="Prep Time (HH:MM)"
+/>
+
               </div>
               <button onClick={() => setNext(true)}
                 type="button"
